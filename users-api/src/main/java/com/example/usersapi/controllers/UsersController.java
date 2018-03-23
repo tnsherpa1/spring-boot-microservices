8dd87d40bc4153e70ae83f60c5632be512218c00
tnsherpa1 <tnsherpa1@gmail.com>
2018-03-22 21:42:29 -0700

    adds CRUD controller to users

---
+++ users-api/src/main/java/com/example/usersapi/controllers/UsersController.java
@@ -0,0 +1,47 @@
+package com.example.usersapi.controllers;
+
+import com.example.usersapi.beans.User;
+import com.example.usersapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UsersController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public Iterable<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{userId}")
+    public User findUserById(@PathVariable Long userId) {
+        return userRepository.findOne(userId);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public HttpStatus deleteUserById(@PathVariable Long userId) {
+        userRepository.delete(userId);
+        return HttpStatus.OK;
+    }
+
+    @PutMapping("/users/{userId}")
+    public User updateUserById(@PathVariable Long userId, @RequestBody User userRequest) {
+
+        User userFromDb = userRepository.findOne(userId);
+
+        userFromDb.setUserName(userRequest.getUserName());
+        userFromDb.setFirstName(userRequest.getFirstName());
+        userFromDb.setLastName(userRequest.getLastName());
+
+        return userRepository.save(userFromDb);
+    }
+}
